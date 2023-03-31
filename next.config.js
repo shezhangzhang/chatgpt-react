dcac6d32f9984650e50365f5a2c42bcbd6841231
Frankie Lin <ljfrankie@gmail.com>
2023-03-31 19:08:29 +0800

    nodejs v16.18.1, run `npm run dev` and access http://localhost:3007 has following error:
    Check your code at header.tsx:34.
    at Header (webpack-internal:///./components/header.tsx:19:13)
    at div
    at div
    at App (webpack-internal:///./pages/_app.tsx:34:16)
    ...
    Warning: React.jsx: type is invalid -- expected a string (for built-in components) or a class/function (for composite components) but got: object.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+const withReactSvg = require("@svgr/webpack");
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+      config.module.rules.push({
+          test: /\.svg$/,
+          use: ["@svgr/webpack"],
+      });
+
+      return config;
+  },
 };
 
 module.exports = nextConfig;
